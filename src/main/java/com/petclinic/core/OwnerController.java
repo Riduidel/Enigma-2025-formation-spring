0914f2c64f0b362896f3863198fb70d4a12d13d7
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2025-04-04 17:10:12 +0200

    ♻️ add request mapping at class level

--- src/main/java/com/petclinic/core/OwnerController.java
+++ src/main/java/com/petclinic/core/OwnerController.java
@@ -8,22 +8,30 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController @RequestMapping("/owner")
 public class OwnerController {
 	private static final Logger logger = LoggerFactory.getLogger(OwnerController.class);
 	@Autowired
 	OwnerService service;
 
-	@GetMapping("/owner/{id}")
+	@GetMapping("/{id}")
 	public Optional<Owner> findById(@PathVariable Long id) {
 		logger.info("Searching for owner with id {}", id);
 		return service.findById(id);
 	}
+	
+	@PostMapping("/")
+	public Owner save(@RequestBody Owner owner) {
+		return service.save(owner);
+	}
 
-	@GetMapping("/owner/search")
+	@GetMapping("/search")
 	public List<Owner> findAllByFirstName(@RequestParam String firstName) {
 		logger.info("Searching for owner with first name {}", firstName);
 		return service.findAllByFirstName(firstName);

d9c2551f66eeeb951ac4e35baf0435dd3fca1676
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2025-04-04 17:04:00 +0200

    End of 9.4 ✨ Search Owner by first name

--- src/main/java/com/petclinic/core/OwnerController.java
+++ src/main/java/com/petclinic/core/OwnerController.java
@@ -1,5 +1,6 @@
 package com.petclinic.core;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +21,11 @@ public class OwnerController {
 	public Optional<Owner> findById(@PathVariable Long id) {
 		logger.info("Searching for owner with id {}", id);
 		return service.findById(id);
+	}
+
+	@GetMapping("/owner/search")
+	public List<Owner> findAllByFirstName(@RequestParam String firstName) {
+		logger.info("Searching for owner with first name {}", firstName);
+		return service.findAllByFirstName(firstName);
 	}
 }
--- src/main/java/com/petclinic/core/OwnerService.java
+++ src/main/java/com/petclinic/core/OwnerService.java
@@ -1,5 +1,6 @@
 package com.petclinic.core;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,9 @@ public class OwnerService {
 
 	public Optional<Owner> findById(Long id) {
 		return ownerRepository.findById(id);
+	}
+
+	public List<Owner> findAllByFirstName(String firstName) {
+		return ownerRepository.findAllByFirstName(firstName);
 	}
 }

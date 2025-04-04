7f60882528f19130f86703754bae980652fbef61
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2025-04-04 16:22:43 +0200

    End of 9.1 🗃️ Inject some default data

---
+++ src/main/java/com/petclinic/tech/DataInitializer.java
@@ -0,0 +1,29 @@
+package com.petclinic.tech;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import com.petclinic.core.Owner;
+import com.petclinic.core.Pet;
+import com.petclinic.core.Visit;
+import com.petclinic.core.VisitService;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class DataInitializer {
+	@Autowired VisitService visitService;
+	@PostConstruct
+	public void initializeSampleData() {
+	    Owner owner = new Owner(null, "joe", "satriani", 1000);
+	    var pet1 = new Pet(null, "dog", "luna");
+	    var visit = new Visit(null, "V01-23", LocalDate.of(2013, 12, 21), "Teeth whitening");
+	    visit.setPet(pet1);
+	    owner.setPets(List.of(pet1));
+	    visit.setOwner(owner);
+	    this.visitService.save(visit);
+	}
+}

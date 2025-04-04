74c599a27b23684455ea548ef3467d5282a37b7f
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2025-04-04 15:17:51 +0200

    End of 8.2 ✨ Added fund transfer support

--- src/main/java/com/petclinic/core/OwnerService.java
+++ src/main/java/com/petclinic/core/OwnerService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class OwnerService {
 	@Autowired OwnerRepository ownerRepository;
@@ -15,5 +17,24 @@ public class OwnerService {
 
 	public Owner save(Owner entity) {
 		return ownerRepository.save(entity);
+	}
+	@Transactional
+	public void transferFunds(Owner ownerToCredit, Owner ownerToDebit, double amount) {
+	    creditAmount(ownerToCredit, amount);
+	    debitAmount(ownerToDebit, amount);
+	}
+
+	private void debitAmount(Owner ownerToDebit, double amount) {
+	    double ownerToDebitNewAmount = ownerToDebit.getAccountStatement() - amount;
+	    if (ownerToDebitNewAmount < 0)
+	        throw new RuntimeException("account value cannot be below zero");
+	    ownerToDebit.setAccountStatement(ownerToDebitNewAmount);
+	    this.ownerRepository.save(ownerToDebit);
+	}
+
+	private void creditAmount(Owner ownerToCredit, double amount) {
+	    double ownerToCreditNewAmount = ownerToCredit.getAccountStatement() + amount;
+	    ownerToCredit.setAccountStatement(ownerToCreditNewAmount);
+	    this.ownerRepository.save(ownerToCredit);
 	}
 }
--- src/test/java/com/petclinic/core/OwnerServiceTransactionsTest.java
+++ src/test/java/com/petclinic/core/OwnerServiceTransactionsTest.java
@@ -1,9 +1,11 @@
 package com.petclinic.core;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -12,7 +14,7 @@ import jakarta.transaction.Transactional;
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SpringBootTest
 class OwnerServiceTransactionsTest {
-
+	@Autowired OwnerService ownerService;
 	@Test void should_not_run_in_transaction() {
 		Assertions.assertThat(TransactionSynchronizationManager.isActualTransactionActive())
 			.describedAs("Method is not annotated @%s, so no transaction should be started", Transactional.class.getSimpleName())
@@ -23,5 +25,25 @@ class OwnerServiceTransactionsTest {
 		Assertions.assertThat(TransactionSynchronizationManager.isActualTransactionActive())
 			.describedAs("Method is annotated @%s, so one transaction should be started", Transactional.class.getSimpleName())
 			.isTrue();
+	}
+
+	@Test
+	public void shouldTransferFunds() {
+		// Given
+	    var ownerToCredit = ownerService.save(new Owner(null, "Jimi","Hendrix", 0));
+	    var ownerToDebit = ownerService.save(new Owner(null, "Robert","Plant",1000));
+		// When
+	    ownerService.transferFunds(ownerToCredit, ownerToDebit, 200);
+	    // Then
+	    Assertions.assertThat(ownerService.findByFirstName("Jimi"))
+	    	.get()
+	    	.extracting(Owner::getAccountStatement)
+	    	.asInstanceOf(InstanceOfAssertFactories.DOUBLE)
+	    	.isEqualTo(200.0, Assertions.byLessThan(0.01));
+	    Assertions.assertThat(ownerService.findByFirstName("Robert"))
+	    	.get()
+	    	.extracting(Owner::getAccountStatement)
+	    	.asInstanceOf(InstanceOfAssertFactories.DOUBLE)
+	    	.isEqualTo(800.0, Assertions.byLessThan(0.01));
 	}
 }
